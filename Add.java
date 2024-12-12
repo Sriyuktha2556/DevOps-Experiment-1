fa033326f74e9ed1e427b8e430a863ae49daa878
sugathri-kadarla <22wh1a0557@bvrithyderabad.edu.in>
2024-12-13 05:13:14 +0530

    java file

--- Add.java
+++ Add.java
@@ -1,10 +1,9 @@
-import java.util.*;
 public class Add{
-   public static void main(String[] args){
-      Scanner sc=new Scanner(System.in);
-      int a=sc.nextInt();
-      int b=sc.nextInt();
-      System.out.print(a+b);
-     }
- }
- 
+  public static void main(String[] args)
+  {
+     Scanner sc=new Scanner(System.in);
+     int a=sc.nextInt();
+     int b=sc.nextInt();
+     System.out.print(a+b);
+  }
+}
