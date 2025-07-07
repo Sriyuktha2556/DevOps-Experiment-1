2f30362a57b3d21d9347956b1af355e26476e420
CharithaBalmuri <22wh1a0548@bvrithyderabad.edu.in>
2025-07-07 08:19:46 +0530

    First Commit

--- Add.java
+++
@@ -1,9 +0,0 @@
-public class Add{
-  public static void main(String[] args)
-  {
-     Scanner sc=new Scanner(System.in);
-     int a=sc.nextInt();
-     int b=sc.nextInt();
-     System.out.print(a+b);
-  }
-}
